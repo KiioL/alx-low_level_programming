31d6a93a6e672be0b2249d2bfeec65ee43c7d077
KiioL <levikitema@gmail.com>
2021-06-27 13:13:10 -0700

    Another one

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-int main(void)/*
- *main - Entry point
- *Prints some text
- *Returns 0
+int main(void)
+/*
+ * main - Entry point
+ * Prints some text
+ * Returns 0
  */
 {
 char words[70] = "\"Programming is like building a multilingual puzzle";
