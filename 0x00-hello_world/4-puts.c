7a3807e13e8b428e36cd0f56b4200f75300be1da
KiioL <levikitema@gmail.com>
2021-06-27 12:33:01 -0700

    Another one

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-int main(void)/*Prints some text to std output*/
+/*
+ *main - Entry point
+ *Prints some text to std output
+ *Return:Always 0
+ */
+int main(void)
 {
-char words[50] = "Programming is like building a multilingual puzzle";
+char words[50] = "Programming is like building a multilingual puzzle\n";
 
 puts(words);
 return (0);
