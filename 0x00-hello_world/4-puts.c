5ce969ac9b3509a9081beb69b84130f0226704f0
KiioL <levikitema@gmail.com>
2021-06-27 12:41:58 -0700

    Another one

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-char words[70] = "Programming is like building a multilingual puzzle\n";
+char words[70] = "\"Programming is like building a multilingual puzzle";
 
 puts(words);
 return (0);
