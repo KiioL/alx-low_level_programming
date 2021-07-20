8f800fa11d365a04fa0ba0e60a5b2b4b8c25a72f
KiioL <levikitema@gmail.com>
2021-07-20 06:43:53 -0700

    Another one

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ * Prints Alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+
+for (x = 'A'; x <= 'Z'; x++)
+{
+int lower_x = tolower(x);
+
+if (lower_x != 'q' && lower_x != 'e')
+{
+putchar(lower_x);
+}
+}
+
+putchar('\n');
+
+return (0);
+
+}
