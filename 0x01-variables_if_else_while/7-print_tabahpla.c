29b31a91d3dabee1ea2f23a75742a5a1860bdd11
KiioL <levikitema@gmail.com>
2021-07-20 07:23:35 -0700

    Another one

---
+++ 0x01-variables_if_else_while/7-print_tabahpla.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ * Prints Alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+
+for (x = 'Z'; x <= 'A'; x--)
+{
+int lower_x = tolower(x);
+
+putchar(lower_x); }
+
+putchar('\n');
+
+return (0);
+
+}
