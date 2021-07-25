450b271ec19022511dfd5e1e11ea1d1441ae7e07
KiioL <levikitema@gmail.com>
2021-07-25 10:41:56 -0700

    Another one

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _islower - Entry Point
+ * Prints alphabet
+ * Return: 1 or 0
+ */
+
+int _islower(int c)
+{
+int x;
+
+if (c > 96 && c < 123)
+{x = 1;}
+else if (c > 64 && c < 91)
+{x = 0;}
+return(x);
+}
+
+
