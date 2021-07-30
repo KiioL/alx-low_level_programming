4f6d8a50451b6c21ec04825c23934c20ef9dfad3
KiioL <levikitema@gmail.com>
2021-07-30 13:27:17 -0700

    Another one

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - Entry Point
+ * Prints time
+ * @void: lowercase version of the parameter
+ * Return: Null
+ */
+
+void jack_bauer(void)
+{
+int x = 0;
+int y = 0;
+
+while (x < 25)
+{
+_putchar((x / 10) + '0');
+_putchar((x % 10) + '0');
+_putchar(58);
+x ++;
+while(y < 60)
+{
+_putchar((y / 10) + '0');
+_putchar((y % 10) + '0');
+_putchar('\n');
+y ++;
+}
+}
+}
+
+
