6fcd34a0cd14a34de8fad5b86cfb33b4b595be6a
KiioL <levikitema@gmail.com>
2021-07-30 04:52:33 -0700

    Another one:

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - Entry Point
+ * Prints last value
+ * @c: lowercase version of the parameter
+ * Return: Always 1 or 0 (success or failure)
+ */
+
+int print_last_digit(int c)
+{
+int x;
+
+
+if (c < 10)
+{
+x = c;
+}
+else
+{
+x = c % 10;
+}
+return (x);
+}
+
+
