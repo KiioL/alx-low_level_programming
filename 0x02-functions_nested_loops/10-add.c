157dd19dc331ff073c4fe6d4949f9041f2000625
KiioL <levikitema@gmail.com>
2021-08-04 06:46:19 -0700

    Another one

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - Entry Point
+ * adds two numbers
+ * @void: lowercase version of the parameter
+ * Return: Interger
+ */
+
+int add(int n, int m)
+{
+int result = n + m;
+
+return (result);
+}
+
+
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code for ALX students
+ * 
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int n;
+
+n = add(89, 9);
+printf("%d\n", n);
+return (0);
+}
+
+
