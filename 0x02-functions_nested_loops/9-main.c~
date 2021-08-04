a9de4f8e581c5d713c2cf33230f212641562e1ef
KiioL <levikitema@gmail.com>
2021-08-04 04:22:35 -0700

    Another one

---
+++ 0x02-functions_nested_loops/9-main.c~
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code for ALX students
+ * 
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+jack_bauer();
+return (0);
+}
+
+
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - Entry Point
+ * Prints 9 times table
+ * @void: lowercase version of the parameter
+ * Return: Null
+ */
+
+void times_table(void)
+{
+
+  int a,b,c;
+
+  for (a = 0;a <=9;a++)
+    {
+      for(b = 0;b <=9;a++)
+	{
+	  c = a * b;
+
+	  if (c < 10)
+	    {
+	      _putchar(c + '0');
+	    }
+	  else
+	    {
+	      _putchar((c / 10) + '0');
+	      _putchar((c % 10) + '0');
+	    }
+	}
+      _putchar('\n');
+    }	 
+}
+
+
