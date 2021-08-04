0fd4fa73e64e78e4e094ded68766ebe73b03494d
KiioL <levikitema@gmail.com>
2021-08-04 06:19:05 -0700

    Another one

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,58 +8,35 @@
  * Return: Null
  */
 
-void times_table(void)
-{
-
+void times_table(void) {
 int a,b,c;
 
-for (a = 0;a <=9;a++)
-{
-for(b = 0;b <=9;b++)
-{
+for (a = 0;a <=9;a++) {
+for(b = 0;b <=9;b++) {
 c = a * b;
 
-if (c < 10)
-{
-if(b < 9)
-{
-if(((b + 1) * a) < 10)
-{
+if (c < 10) {
+if(b < 9) {
+if(((b + 1) * a) < 10) {
 _putchar(c + '0');
 _putchar(44);
 _putchar(32);
-_putchar(32);
-}
-else
-{
+_putchar(32); }
+else {
 _putchar(c + '0');
 _putchar(44);
-_putchar(32);
-}
-}
-else
-{
-_putchar(c + '0');
-}
-}
-else
-{
-if(b < 9)
-{
+_putchar(32); }}
+else {
+_putchar(c + '0'); }}
+else {
+if(b < 9) {
 _putchar((c / 10) + '0');
 _putchar((c % 10) + '0');
 _putchar(44);
-_putchar(32);
-}
-else
-{
+_putchar(32); }
+else {
 _putchar((c / 10) + '0');
-_putchar((c % 10) + '0');
-}
-}
-}
-_putchar('\n');
-}
-}
+_putchar((c % 10) + '0'); }}}
+_putchar('\n'); }}
 
 
