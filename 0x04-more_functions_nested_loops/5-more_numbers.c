ac5d3676f973e4edd25715119e8ee5b725541c7e
KiioL <levikitema@gmail.com>
2021-08-05 08:51:15 -0700

    Another one

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,15 +17,14 @@ for (b = 0; b <= 9; b++)
 {
 for (a = 0; a <= 14; a++)
 {
-if (a < 10)
-{
-_putchar(a + '0');
-}
-else
+
+if (a > 9)
 {
 _putchar((a / 10) + '0');
-_putchar((a % 10) + '0');
 }
+
+_putchar((a % 10) + '0');
+
 }
 _putchar('\n');
 }
