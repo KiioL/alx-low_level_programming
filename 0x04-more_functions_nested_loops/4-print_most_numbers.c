1fb31286b907281651b9cee7784fa8071ab6e084
KiioL <levikitema@gmail.com>
2021-08-05 07:32:34 -0700

    Another one

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ * Prints
+ * @void: nothing
+ * Return: Void
+ */
+
+void print_most_numbers(void)
+{
+int a;
+
+for (a = 48; a <= 57; a++)
+{
+if (a != 50 && a != 52)
+{
+_putchar(a);
+}
+}
+_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c~
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ * Prints
+ * @void: nothing
+ * Return: Void
+ */
+
+void print_numbers(void)
+{
+int a;
+
+for (a = 48; a <= 57; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
