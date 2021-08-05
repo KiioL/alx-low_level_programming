28fe294ab9a68cd6634908b344b4fac0d587872a
KiioL <levikitema@gmail.com>
2021-08-05 05:16:30 -0700

    Another one

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,15 +9,15 @@
 
 int _isupper(int c)
 {
-  int a;
+int a;
   
-  if (c > 65 && c < 91)
-    {
-      a = 1;
-    }
-  else if (c > 96 && c < 123)
-    {
-      a = 0;
-    }
-  return (a);
+if (c > 64 && c < 91)
+{
+a = 1;
+}
+else if (c > 96 && c < 123)
+{
+a = 0;
+}
+ return (a);
 }
