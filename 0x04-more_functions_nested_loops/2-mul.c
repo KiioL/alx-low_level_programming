b30bb5d0381c9dca89cf5b99f7a969c643cf1a27
KiioL <levikitema@gmail.com>
2021-08-05 06:54:06 -0700

    Another one

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - Entry point
+ * Multiplies
+ * @a: integer parameter
+ * @b: integer parameter
+ * Return: Interger
+ */
+
+int mul(int a, int b)
+{
+int c = a * b;
+
+return (c);
+}
