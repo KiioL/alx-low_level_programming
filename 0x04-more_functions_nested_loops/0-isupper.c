7332f974122be89ccdc70b382d0182e07436bb02
KiioL <levikitema@gmail.com>
2021-08-05 05:22:24 -0700

    Another one

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,21 +3,21 @@
 /**
  * _isupper - Entry point
  * Checks for upper case
- * @c: Parameter
- * Return : Interger
+ * @c: integer parameter
+ * Return: Interger
  */
 
 int _isupper(int c)
 {
 int a;
-  
+
 if (c > 64 && c < 91)
 {
 a = 1;
 }
-else if (c > 96 && c < 123)
+else
 {
 a = 0;
 }
- return (a);
+return (a);
 }
