33bcc8f6d03a58badb87a6d879ab1a6e1cac8c71
KiioL <levikitema@gmail.com>
2021-08-09 03:41:15 -0700

    Another one

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,13 +4,13 @@
 /**
  * print_line - Entry point
  * Prints
- * @void: nothing
+ * @n: parameter
  * Return: Void
  */
 
 void print_line(int n)
 {
-if ( n <= 0)
+if (n <= 0)
 {
 _putchar('\n');
 }
@@ -19,6 +19,7 @@ else
 while (n > 0)
 {
 _putchar(95);
+n--;
 }
 _putchar('\n');
 }
