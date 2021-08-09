2feb17668bb5f4095b66c3786e69050c790e0330
KiioL <levikitema@gmail.com>
2021-08-09 06:22:32 -0700

    Another one

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - Entry point
+ * Prints
+ * @n: parameter
+ * Return: Void
+ */
+
+void print_diagonal(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int z;
+
+for (z = 0; z < n; z++)
+{
+int x = 0;
+
+while (x <= z)
+{
+_putchar(32);
+x++;
+}
+_putchar(92);
+_putchar('\n');
+}
+}
+}
