ad334fd9114ffe9899529d9571988cae150a04dd
KiioL <levikitema@gmail.com>
2021-08-12 04:54:47 -0700

    Another one

---
+++ 0x04-more_functions_nested_loops/9-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+#include "main.h"
+
+/**
+ * main - check the code for ALX School students.
+ * 
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+  fizz_buzz();
+  return (0);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -7,3 +7,4 @@ int mul(int a, int b);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void fizz_buzz(void);
